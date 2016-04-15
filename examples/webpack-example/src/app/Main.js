f5d4bc24fe95faf07d68c9dda42418af70693f76
Conor Fennell <fennell.conor@gmail.com>
2016-04-15 11:34:58 +0100

    updated example prpject to use latest version

--- examples/webpack-example/src/app/Main.js
+++ examples/webpack-example/src/app/Main.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import Conversation from 'chat-template/Conversation';
 
-const messages = [{
-  message: 'hey how are you',
-  duration: 1000,
-  from: 'left',
-  backColor: 'white'
-},
-{
-  message: 'hey how are you',
-  duration: 1000,
-  from: 'right',
-  backColor: 'lightgreen'
-}]
+const messages = [
+      {
+        type: 'typing',
+        inbound: true,
+        duration: 2000
+      },
+      {
+        message:'Hey, how are you getting on?',
+        from: 'left',
+        backColor: 'whitergb(250, 250, 250)',
+        avatar: 'https://landofblogging.files.wordpress.com/2014/01/bitstripavatarprofilepic.jpeg',
+        duration: 1000
+      },
+      {
+        type: 'typing',
+        inbound: true,
+        duration: 1000
+      },
+      {
+        message:'It has been ages since we talked.',
+        from: 'left',
+        backColor: 'whitergb(250, 250, 250)',
+        avatar: 'https://landofblogging.files.wordpress.com/2014/01/bitstripavatarprofilepic.jpeg',
+        duration: 2000
+      },
+      {
+        message:'How do I uninstall this messaging app?',
+        from: 'right',
+        backColor: '#3d83fa',
+        textColor: "white",
+        avatar: 'https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/andrew.png',
+        duration: 2000
+      },
+      {
+        type: 'typing',
+        inbound: true,
+        duration: 2000
+      },
+      {
+        src: 'https://media.giphy.com/media/xT1XGLzxTFgIwxcbcY/giphy.gif',
+        from: 'left',
+        backColor: 'whitergb(250, 250, 250)',
+        avatar: 'https://landofblogging.files.wordpress.com/2014/01/bitstripavatarprofilepic.jpeg',
+        duration: 2500
+        }
+    ];
 
 const styles = {
-  width: "200px",
-  height: "50%"
-}
-
-const stylesDuvGeight = {
-  height: "50%"
+  width: "400px"
 }
 
 class Main extends React.Component {
@@ -28,10 +57,7 @@ class Main extends React.Component {
     return (
       <div style={styles}>
         <div style={styles}>
-        	<Conversation messages={messages}/>
-        </div>
-        <div style={stylesDuvGeight}>
-          <p> Howdy jvkxhvhalhludhlhsfalduha</p>
+        	<Conversation typingColor={"#3d83fa"} height={300} messages={messages}/>
         </div>
       </div>
     );
