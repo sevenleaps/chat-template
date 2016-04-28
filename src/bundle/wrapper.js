9c6ed1b4569e8ed9767fb6b75449a1b279f70674
witnessmenow <ecenuig@gmail.com>
2016-04-28 10:48:48 +0100

    Fixing lint errors

--- src/bundle/wrapper.js
+++ src/bundle/wrapper.js
@@ -1,8 +1,8 @@
-import {Conversation} from '../index';
+import { Conversation } from '../index';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-const ShowChatTemplate = (messages, element, delay=1, height=300) => {
+const ShowChatTemplate = (messages, element, delay = 1, height = 300) => {
   ReactDOM.render(<Conversation delay={delay} height={height} messages={messages} />,
     element);
 };
