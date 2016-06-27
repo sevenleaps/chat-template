3931a3d5c8168d193476da6d12b5ca1884dcbe98
Guns <guns2410@users.noreply.github.com>
2016-06-28 03:32:31 +0530

    Fix showMessage
    Fix showMessage function declaration

--- src/Conversation.js
+++ src/Conversation.js
@@ -60,7 +60,7 @@ class Conversation extends React.Component {
     clearTimeout(this.timeoutId);
   }
 
-  showMessage = () => {
+  showMessage() {
     const messages = this.state.messages;
     const messagesToBeDisplayed = this.state.messagesToBeDisplayed;
     const reset = this.state.reset;
